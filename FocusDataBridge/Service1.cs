d1fa2b00230bfae80436fd4c8d7034d7253880dd
orochigalois <fudanyinxin@gmail.com>
2016-10-06 21:22:19 +0530

    初步完成连接sql server及mysql功能 #1

--- FocusDataBridge/Service1.cs
+++ FocusDataBridge/Service1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace FocusDataBridge
 {
@@ -24,7 +27,54 @@ namespace FocusDataBridge
 
         protected override void OnStart(string[] args)
         {
+            string last="", first="";
+            DBConnect dbConnect;
+            SqlDataReader rdr = null;
             System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+            SqlConnection con = new SqlConnection("Data Source=DBServer;Password=Miaoyaliu1213;Persist Security Info=True;User ID=sa;Initial Catalog=BPSPatients;Data Source=CND98GRZ52\\SQLEXPRESS");
+            try
+            {
+                con.Open();
+      
+
+                SqlCommand cmd = new SqlCommand(
+                "BP_GetAllUsers", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    rdr=cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        // get the results of each column
+                        last = (string)rdr["SURNAME"];
+                        first = (string)rdr["FIRSTNAME"];
+                        string email = (string)rdr["EMAIL"];
+
+                        // print out the results
+
+
+                        using (StreamWriter writer = new StreamWriter("c:\\OnStart.txt", true))
+                        {
+                            writer.WriteLine(first + ";" + last + ";" + email + "\n");
+                            writer.Close();
+                        }
+
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            dbConnect = new DBConnect();
+            dbConnect.Insert(first+last);
         }
 
         protected override void OnStop()
