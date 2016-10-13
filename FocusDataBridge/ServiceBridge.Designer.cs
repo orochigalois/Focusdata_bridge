1c10135a83b385d9ddfc693deb653e30ffd9ac81
orochigalois <fudanyinxin@gmail.com>
2016-10-13 21:21:03 +0530

    5s轮询数据库 #4

--- FocusDataBridge/ServiceBridge.Designer.cs
+++ FocusDataBridge/ServiceBridge.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // ServiceBridge
+            // 
             this.ServiceName = "ServiceBridge";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
         }
 
         #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
     }
 }
--- FocusDataBridge/ServiceBridge.cs
+++ FocusDataBridge/ServiceBridge.cs
@@ -10,11 +10,16 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.Threading;
 
 namespace FocusDataBridge
 {
     public partial class ServiceBridge : ServiceBase
     {
+        private CancellationTokenSource cts = new CancellationTokenSource();
+        private Task mainTask = null;
+
+
         public ServiceBridge()
         {
             InitializeComponent();
@@ -27,59 +32,103 @@ namespace FocusDataBridge
 
         protected override void OnStart(string[] args)
         {
-            string last="", first="";
-            DBConnect dbConnect;
-            SqlDataReader rdr = null;
-            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
-            SqlConnection con = new SqlConnection("Data Source=DBServer;Password=Miaoyaliu1213;Persist Security Info=True;User ID=sa;Initial Catalog=BPSPatients;Data Source=CND98GRZ52\\SQLEXPRESS");
-            try
+
+            mainTask = new Task(Poll, cts.Token, TaskCreationOptions.LongRunning);
+            mainTask.Start();
+
+            
+        }
+
+        private void Poll()
+        {
+            CancellationToken cancellation = cts.Token;
+           // TimeSpan interval = TimeSpan.Zero;
+            TimeSpan interval = TimeSpan.FromSeconds(5);
+
+
+            while (!cancellation.WaitHandle.WaitOne(interval))
             {
-                con.Open();
-      
-
-                SqlCommand cmd = new SqlCommand(
-                "BP_GetAllUsers", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-
                 try
                 {
-                    rdr=cmd.ExecuteReader();
-                    while (rdr.Read())
+                    // Put your code to poll here.
+                    // Occasionally check the cancellation state.
+
+                    string last = "", first = "";
+                    DBConnect dbConnect;
+                    SqlDataReader rdr = null;
+                    //System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+                    SqlConnection con = new SqlConnection("Data Source=DBServer;Password=Miaoyaliu1213;Persist Security Info=True;User ID=sa;Initial Catalog=BPSPatients;Data Source=CND98GRZ52\\SQLEXPRESS");
+                    try
                     {
-                        // get the results of each column
-                        last = (string)rdr["SURNAME"];
-                        first = (string)rdr["FIRSTNAME"];
-                        string email = (string)rdr["EMAIL"];
-
-                        // print out the results
+                        con.Open();
 
 
-                        using (StreamWriter writer = new StreamWriter("c:\\OnStart.txt", true))
+                        SqlCommand cmd = new SqlCommand(
+                        "BP_GetAllUsers", con);
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        try
                         {
-                            writer.WriteLine(first + ";" + last + ";" + email + "\n");
-                            writer.Close();
+                            rdr = cmd.ExecuteReader();
+                            while (rdr.Read())
+                            {
+                                // get the results of each column
+                                last = (string)rdr["SURNAME"];
+                                first = (string)rdr["FIRSTNAME"];
+                                string email = (string)rdr["EMAIL"];
+
+                                // print out the results
+
+
+                                //using (StreamWriter writer = new StreamWriter("c:\\OnStart.txt", true))
+                                //{
+                                //    writer.WriteLine(first + ";" + last + ";" + email + "\n");
+                                //    writer.Close();
+                                //}
+
+                            }
                         }
-
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
                     }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+
+                    dbConnect = new DBConnect();
+                    dbConnect.Insert(first + last);
+
+
+
+
+                    if (cancellation.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                    interval = TimeSpan.FromSeconds(5);
                 }
-                catch(Exception e)
+                catch (Exception ex)
                 {
-                    Console.WriteLine(e.Message);
+                    // Log the exception.
+
+                    eventLog1.WriteEntry("Bridge encountered an error '" +
+                    ex.Message + "'", EventLogEntryType.Error);
+
+                    interval = TimeSpan.FromSeconds(5);
                 }
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-
-            dbConnect = new DBConnect();
-            dbConnect.Insert(first+last);
         }
 
         protected override void OnStop()
         {
-            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStopAlex.txt");
+            //System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStopAlex.txt");
+
+            cts.Cancel();
+            mainTask.Wait();
         }
     }
 }
